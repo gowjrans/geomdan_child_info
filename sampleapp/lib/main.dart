85e580601e981197a0781bf4fc6839ec5b8dc84a
gowjrans.kim <gowjrans.kim@lgapps.lge.com>
2023-07-20 16:15:36 +0900

    230720

--- sampleapp/lib/main.dart
+++ sampleapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const BottomNavigationBarApp());
 
@@ -119,10 +120,13 @@ class ProgressListBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
+          Padding(padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            child: Text("안전한 집 찾기 안전집사가 함께해요.", style: TextStyle(fontSize: 25),),
+          ),
         ],
       ),
     );
@@ -138,19 +142,35 @@ class ChatBody extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Padding(
+          const Padding(
             padding: EdgeInsets.fromLTRB(20, 10, 10, 0),
             child: Row(
               children: [
-                Icon(Icons.forum, color: Colors.teal,),
+                Icon(Icons.forum, color: Color(0xFF088A85),),
                 SizedBox(width: 10, height: 0,),
-                Text("아직 중개사님이 작성한 댓글이 없어요", style : TextStyle(color : Colors.teal)),
+                Text("아직 중개사님이 작성한 댓글이 없어요", style : TextStyle(fontSize:19, color : Color(0xFF088A85))),
               ],
             ),
           ),
-          TextButton(
-            onPressed: (){},
-            child: Text("작성한 댓글 없음", style: TextStyle(color: Color(0xffb2babb)),), style: TextButton.styleFrom(fixedSize:Size(300, 100) ,backgroundColor: Color(0xfff4f6f7),),
+          Container(
+            margin: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            width: double.infinity,
+            child: const Text("작성한 댓글 없음", style: TextStyle(fontSize: 16, color: Colors.grey),  textAlign: TextAlign.center),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(30, 10, 20, 30),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: const Color(0xFFF8ECE0),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            width: double.infinity,
+            child: const Text("문의한 매물 채팅도 현재 화면에서 가능하도록 곧 업데이트 할 예정입니다. 문의한 매물 채팅은 하단의 진행목록 탭을 통해 진행해주세요.", style: TextStyle(fontSize: 19, color: Color(0xFF886A08)),  textAlign: TextAlign.center),
           ),
         ],
       ),
@@ -164,12 +184,74 @@ class MyInfoBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-          children: [
-          Text("내 정보"),
-      ],
-    ),
+        children: [
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("내 정보", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 30,),
+                const Text("내이름", style: TextStyle(fontSize: 22),),
+                const SizedBox(height: 25,),
+                GestureDetector(
+                  onTap: (){
+                    _launchURL();
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.black26),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.assignment_outlined, color: Colors.lightBlueAccent,),
+                            SizedBox(width: 10,),
+                            Text("안전집사 보증보험 리포트 신청하기", style: TextStyle(fontSize: 14),),
+                            Icon(Icons.chevron_right,),
+                          ],
+                        ),
+                        Text("보험 가능여부, 오늘 기준 알려드려요! (앱에 없는 집 가능)",style: TextStyle(fontSize: 12, color: Colors.grey),)
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),Container(width: double.infinity, height: 10, color: Color(0XFFF2F2F2),),
+        ],
+      ),
     );
+  }
+}
+
+Widget getListItem(String title, bool logout){
+  if(logout) {
+    return Row(
+      children: [
+        Text(title),
+        Expanded(child: Container(),),
+        Icon(Icons.chevron_right),
+      ],
+    );
+  } else {
+    return Text(title);
+  }
+}
+
+_launchURL() async {
+  const url = 'https://safejibsa.com/?open=1';
+  final uri = Uri.parse(url);
+  if (await canLaunchUrl(uri)) {
+    await launchUrl(uri);
+  } else {
+    throw 'Could not launch $url';
   }
 }
